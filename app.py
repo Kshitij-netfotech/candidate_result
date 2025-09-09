880997d9b0fc0eea490bdf37325d660b0af5fc43
Kshitij Kediya <kshitij@netfotech.in>
2025-09-09 16:33:53 +0530

    added cors

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     from results.controller import bp as results_bp
     app.register_blueprint(results_bp)
 
